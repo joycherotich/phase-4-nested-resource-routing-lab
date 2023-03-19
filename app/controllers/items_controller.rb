58ec5be278f9290c17ffde3c650e1b4ec459df1c
joy <letimjoy7@gmail.com>
2023-03-19 07:09:25 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
+  before_action :set_user
 
   def index
     items = Item.all
     render json: items, include: :user
   end
 
+  def show
+    @item = @user.items.find_by(id: params[:id])
+    render json: @item
+  end
+
+  def create
+    @item = @user.items.new(item_params)
+    if @item.save
+      render json: @item, status: :created
+    else
+      render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "404" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: { error: "404" }, status: :not_found
+  end
 end
